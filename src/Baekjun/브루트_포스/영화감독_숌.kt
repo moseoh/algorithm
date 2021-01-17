d3e7cb4aa3a91069bd84772820ff7a7774878ca9
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-01-18 04:00:15 +0900

    영화감독 숌

---
+++ src/Baekjun/브루트_포스/영화감독_숌.kt
@@ -0,0 +1,16 @@
+package src.Baekjun.브루트_포스
+
+fun main() {
+    val N = readLine()!!.toInt()
+    var count = 1
+    var num = 666
+
+    while(N > count){
+        num++
+        if(num.toString().contains("666")){
+            count++
+        }
+    }
+    println(num)
+
+}
