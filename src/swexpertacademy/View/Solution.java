41bf4874f3446af5e511ced63ea4020234fdf582
moseoh <azqazq195@gmail.com>
2021-03-10 07:29:05 +0900

    View

--- src/swexpertacademy/View/Solution.java
+++ src/swexpertacademy/View/Solution.java
@@ -8,6 +8,27 @@ class Solution {
     public static void main(String args[]) throws Exception {
         System.setIn(new FileInputStream("src/swexpertacademy/View/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 0 0 5 4 7 8 2 0 0
+        // 0 0 0 0 0 1 0 0 0
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            int[] building = new int[Integer.parseInt(br.readLine())];
+            String[] temp = br.readLine().split(" ");
+            int result = 0;
 
+            for (int i = 0; i < building.length; i++) {
+                building[i] = Integer.parseInt(temp[i]);
+            }
+
+            for (int i = 2; i < building.length - 2; i++) {
+                int max = 0;
+                if(building[i-2] > max) max = building[i-2];
+                if(building[i-1] > max) max = building[i-1];
+                if(building[i+1] > max) max = building[i+1];
+                if(building[i+2] > max) max = building[i+2];
+                if(building[i] > max) result = result + building[i] - max;
+            }
+
+            System.out.println("#" + test_case + " " + result);
+        }
     }
 }
