c49006ae1278088aa2b74abc469f1c3c167ee0a2
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-03-09 21:03:25 +0900

    Bubble Sort

---
+++ src/concept/BubbleSort.java
@@ -0,0 +1,33 @@
+package src.concept;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {55, 7, 78, 12, 42};
+
+        bubbleSortASC(arr);
+    }
+
+    static void bubbleSortASC(int[] arr) {
+        System.out.println("Start");
+        printArr(arr);
+        for( int x = arr.length; x > 0; x--){
+            for (int i = 0; i < x - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+                printArr(arr);
+            }
+            System.out.println();
+        }
+    }
+
+    static void printArr(int[] arr) {
+        System.out.print("arr: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
