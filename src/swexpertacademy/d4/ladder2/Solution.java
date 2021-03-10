e1f5796a715312dba186509ae3ea78b53c2d28a9
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-03-11 05:35:28 +0900

    ladder1, ladder2

---
+++ src/swexpertacademy/d4/ladder2/Solution.java
@@ -0,0 +1,12 @@
+package src.swexpertacademy.d4.ladder2;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        System.setIn(new FileInputStream("src/swexpertacademy/d4/ladder2/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
