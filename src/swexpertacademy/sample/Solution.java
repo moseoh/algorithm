526d76ebb2f460c942863737b9abf8f2f2b8df15
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-03-11 19:08:26 +0900

    Sample fix

--- src/swexpertacademy/sample/Solution.java
+++ src/swexpertacademy/sample/Solution.java
@@ -8,5 +8,12 @@ class Solution {
     public static void main(String args[]) throws Exception {
         System.setIn(new FileInputStream("src/swexpertacademy/d3/패키지명/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            sb.append("#").append(test_case).append(" ");
+
+            sb.append("answer").append("\n");
+        }
+        System.out.println(sb);
     }
 }
