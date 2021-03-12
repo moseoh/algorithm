9a612924707ffd74ae479108e4d6e4214a7ff1af
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-03-12 12:40:07 +0900

    Stack

---
+++ src/개념/stack/ArrayStack.java
@@ -0,0 +1,35 @@
+package src.개념.stack;
+
+public class ArrayStack {
+    static int[] stack = new int[100];
+    static int top = -1;
+
+    public static void main(String[] args) {
+        int item;
+
+        push(1);
+        push(2);
+        push(3);
+
+        item = pop();
+        System.out.println("pop item => " + item);
+        item = pop();
+        System.out.println("pop item => " + item);
+        item = pop();
+        System.out.println("pop item => " + item);
+    }
+
+    static void push(int item){
+        if (top < 99) {
+            stack[++top] = item;
+        }
+    }
+
+    static int pop() {
+        if(top == -1) {
+            System.out.println("Stack is Empty!!");
+            return 0;
+        }
+        else return stack[top--];
+    }
+}
---
+++ src/개념/stack/ListStack.java
@@ -0,0 +1,36 @@
+package src.개념.stack;
+
+import java.util.ArrayList;
+
+public class ListStack {
+    static ArrayList<Integer> stack = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int item;
+
+        push(1);
+        push(2);
+        push(3);
+
+        pop();
+        pop();
+        pop();
+        pop();
+    }
+
+    static void push(int item) {
+        stack.add(item);
+        System.out.println("push item => " + item);
+    }
+
+    static void pop() {
+        if (stack.size() == 0) {
+            System.out.println("Stack is Empty!!");
+        } else {
+            int item = stack.get(stack.size() - 1);
+            stack.remove(stack.size() - 1);
+            System.out.println("pop item => " + item);
+        }
+    }
+
+}
