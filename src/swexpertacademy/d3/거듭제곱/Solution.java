285cfb84bd94dcbe38ca1e5e548032526f10705a
moseoh <azqazq195@gmail.com>
2021-03-14 15:57:28 +0900

    거듭제곱

--- src/swexpertacademy/d3/거듭제곱/Solution.java
+++ src/swexpertacademy/d3/거듭제곱/Solution.java
@@ -11,9 +11,16 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         for (int test_case = 1; test_case <= 10; test_case++) {
             sb.append("#").append(test_case).append(" ");
-
-            sb.append("answer").append("\n");
+            br.readLine();
+            String[] temp = br.readLine().split(" ");
+            int num = Integer.parseInt(temp[0]);
+            int power = Integer.parseInt(temp[1]);
+            sb.append(power(num, power)).append("\n");
         }
         System.out.println(sb);
+    }
+    static int power(int num, int power){
+        if(power == 0) return 1;
+        return num * power(num, power - 1);
     }
 }
