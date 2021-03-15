ef08a1504e6b8ce0c443fd8eaede3dba4811ea5e
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-03-16 04:08:34 +0900

    계산기2

--- src/Temp.java
+++ src/Temp.java
@@ -6,12 +6,12 @@ public class Temp {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
-        System.out.println(stack.pop());
-        try {
-            System.out.println(stack.pop());
-        } catch (Exception e){
-            System.out.println("에러");
-        }
-        
+        stack.push(1);
+        stack.push(1);
+        System.out.println(stack.size());
+
+        while(!stack.isEmpty()) stack.pop();
+        System.out.println(stack.size());
+
     }
 }
