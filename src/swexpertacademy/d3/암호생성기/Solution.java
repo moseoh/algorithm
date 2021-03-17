be2e7ff76fecf73370d8f6453bc8e8e931421287
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-03-17 14:55:04 +0900

    3 Queue Problem Frame

---
+++ src/swexpertacademy/d3/암호생성기/Solution.java
@@ -0,0 +1,19 @@
+package src.swexpertacademy.d3.암호생성기;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        System.setIn(new FileInputStream("src/swexpertacademy/d3/암호생성기/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            sb.append("#").append(test_case).append(" ");
+
+            sb.append("answer").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
---
+++ src/swexpertacademy/d4/미로2/Solution.java
@@ -0,0 +1,19 @@
+package src.swexpertacademy.d4.미로2;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+class Solution {
+    public static void main(String args[]) throws Exception {
+        System.setIn(new FileInputStream("src/swexpertacademy/d4/미로2/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            sb.append("#").append(test_case).append(" ");
+
+            sb.append("answer").append("\n");
+        }
+        System.out.println(sb);
+    }
+}
