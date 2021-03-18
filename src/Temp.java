8d69a3343c57ecbb75963a2641660ae55f430b5b
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-03-18 22:55:08 +0900

    모의 테스트

--- src/Temp.java
+++ src/Temp.java
@@ -1,28 +1,66 @@
 package src;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Temp {
-    public static void main(String[] args) {
-        List<Integer> list = new LinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-
-        for(Integer integer : list) {
-            System.out.print(integer + " ");
+    public static void main(String[] args) throws Exception {
+        // n, k, 배열
+        // 입력 받기
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        String[] input2 = br.readLine().split(" ");
+        int length = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        int[] num = new int[length];
+        for (int i = 0; i < length; i++) {
+            num[i] = Integer.parseInt(input2[i]);
         }
-        System.out.println();
 
-        list.add(1, 7);
-        list.add(1, 6);
-        list.add(1, 5);
-
-        for(Integer integer : list) {
-            System.out.print(integer + " ");
+        int min = 0;
+        int index = 0;
+        for (int i = 0; i < length; i++) {
+            if(i == 0) {
+                min = num[i];
+                index = i;
+                continue;
+            }
+            if(min > num[i]) {
+                min = num[i];
+                index = i;
+            }
         }
+
+        int result = 0;
+        for(int i = index; i > index - k; i--){
+//            System.out.println("index: " + i);
+            if(i < 0) break;
+            int count = 0;
+            int temp = i;
+            while(temp > 0){
+                temp -= (k-1);
+                count++;
+//                System.out.print("왼 ");
+            }
+
+            temp = i;
+            while(temp < length - 1){
+                temp += (k-1);
+                count++;
+//                System.out.print("오 ");
+            }
+//            System.out.println("count: " + count);
+            if(i == index) result = count;
+            if(result > count) result = count;
+        }
+
+//        for (int i : num) {
+//            System.out.print(i + " ");
+//        }
+//        System.out.println();
+//        System.out.println("min: " + min);
+//        System.out.println("index: " + index);
+//        System.out.println("result: " + result);
+        System.out.println(result);
     }
 }
---
+++ src/개념/Temp3.java
@@ -0,0 +1,7 @@
+package src.개념;
+
+public class Temp3 {
+    public static void main(String[] args) {
+        System.out.println(4/5);
+    }
+}
