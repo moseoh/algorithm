5c25eb4b02439541e500346a3c1e4f842f13dcd4
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-03-18 17:04:24 +0900

    Merge Sort

---
+++ src/개념/sort/MergeSort.java
@@ -0,0 +1,88 @@
+package src.개념.sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(69);
+        list.add(10);
+        list.add(30);
+        list.add(2);
+        list.add(16);
+        list.add(8);
+        list.add(31);
+        list.add(22);
+
+        System.out.println("정렬 전 => ");
+        for(Integer integer: list) {
+            System.out.print(integer + " ");
+        }
+        System.out.println();
+        System.out.println("정렬 후 => ");
+        list = mergeSort(list);
+        for(Integer integer: list) {
+            System.out.print(integer + " ");
+        }
+    }
+
+    static List<Integer> mergeSort(List<Integer> list) {
+        if (list.size() <= 1) {
+            return list;
+        }
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+
+        for (int i = 0; i < list.size() / 2; i++) {
+            left.add(list.get(i));
+        }
+        for (int i = list.size() / 2; i < list.size(); i++) {
+            right.add(list.get(i));
+        }
+
+        // List 의 크기가 1이 될 때까지 분할
+        left = mergeSort(left);
+        right = mergeSort(right);
+
+        // 분할된 List 들 병합
+        return merge(left, right);
+    }
+
+    static List<Integer> merge(List<Integer> left, List<Integer> right) {
+        List<Integer> result = new ArrayList<>();
+
+        while(left.size() > 0 && right.size() > 0){
+            if(left.get(0) <= right.get(0)){
+                result.add(left.get(0));
+                left.remove(0);
+            } else {
+                result.add(right.get(0));
+                right.remove(0);
+            }
+        }
+
+        if(left.size() >0){
+            while(left.size() > 0) {
+                result.add(left.get(0));
+                left.remove(0);
+            }
+        }
+        if(right.size() >0){
+            while(right.size() > 0) {
+                result.add(right.get(0));
+                right.remove(0);
+            }
+        }
+
+        return result;
+    }
+
+    static void printArr(String name, int[] arr) {
+        System.out.print(name + ":\t");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
