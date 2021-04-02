a254c965bfb303b4ce3dd90fa1652e47c7b586ab
moseoh <45132207+azqazq195@users.noreply.github.com>
2021-04-02 15:43:06 +0900

    Update

--- src/baekjun/동적계획법/평범한배낭.java
+++ src/baekjun/동적계획법/평범한배낭.java
@@ -16,22 +16,26 @@ public class 평범한배낭 {
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] stuff = new int[n + 1][2];
+
+        int[] w = new int[n + 1];
+        int[] v = new int[n + 1];
         int[][] dp = new int[n + 1][k + 1];
-        for (int i = 1; i <= n; i++) {
+
+        for(int i = 1 ; i <= n; i++) {
             st = new StringTokenizer(br.readLine(), " ");
-            stuff[i][0] = Integer.parseInt(st.nextToken());
-            stuff[i][1] = Integer.parseInt(st.nextToken());
+            w[i] =  Integer.parseInt(st.nextToken());
+            v[i] =  Integer.parseInt(st.nextToken());
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= k; j++) {
+        for(int i = 1 ; i <= n ; i++) {
+            for(int j = 1;  j <= k; j++) {
                 dp[i][j] = dp[i-1][j];
-                if(stuff[i][0] <= j) {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-stuff[i][0]] + stuff[i][1]);
-                }
+                if(w[i] <= j) dp[i][j] = Math.max(dp[i-1][j], v[i] + dp[i-1][j-w[i]]);
             }
         }
+
         System.out.println(dp[n][k]);
+
+
     }
 }
