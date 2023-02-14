b827c4acd176a75da8fb895e623d16d92618692d
moseoh <azqazq195@gmail.com>
2023-02-14 17:48:19 +0900

    feat: Level0 푸드파이트 대회

---
+++ intellij/src/programmers/푸드파이트대회/Solution.kt
@@ -0,0 +1,19 @@
+package src.programmers.푸드파이트대회
+
+fun main(args: Array<String>) {
+    val food = intArrayOf(1, 7, 1, 2)
+    println(Solution().solution(food))
+}
+
+
+class Solution {
+    fun solution(food: IntArray): String {
+        val sb = StringBuilder()
+        for (i in 1..food.lastIndex)
+            repeat(food[i] / 2) { sb.append(i) }
+        val reversed = sb.reversed()
+        sb.append(0)
+        sb.append(reversed)
+        return sb.toString()
+    }
+}
