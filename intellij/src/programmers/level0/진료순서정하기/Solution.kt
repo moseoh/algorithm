2fff65d9bac469baeea57c9934221beb83891c63
moseoh <azqazq195@gmail.com>
2023-02-14 17:47:52 +0900

    feat: Level0 진료순서 정하기

---
+++ intellij/src/programmers/level0/진료순서정하기/Solution.kt
@@ -0,0 +1,16 @@
+package src.programmers.level0.진료순서정하기
+
+fun main(args: Array<String>) {
+    val emergency = intArrayOf(3, 76, 24)
+    println(Solution().solution(emergency).contentToString())
+}
+
+
+class Solution {
+    fun solution(emergency: IntArray): IntArray {
+        val sorted = emergency.sortedDescending()
+        return emergency.map {
+            sorted.indexOf(it) + 1
+        }.toIntArray()
+    }
+}
