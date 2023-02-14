29cf221ef954427c309e8236f063ce74bffd46a9
moseoh <azqazq195@gmail.com>
2023-02-14 17:48:28 +0900

    feat: Level0 행렬의 덧셈

---
+++ intellij/src/programmers/행렬의덧셈/Solution.kt
@@ -0,0 +1,19 @@
+package src.programmers.행렬의덧셈
+
+fun main(args: Array<String>) {
+    val arr1 = arrayOf(intArrayOf(1, 2), intArrayOf(2, 3))
+    val arr2 = arrayOf(intArrayOf(3, 4), intArrayOf(5, 6))
+
+//    val arr1 = arrayOf(intArrayOf(1), intArrayOf(2))
+//    val arr2 = arrayOf(intArrayOf(3), intArrayOf(4))
+    println(Solution().solution(arr1, arr2).contentToString())
+}
+
+
+class Solution {
+    fun solution(arr1: Array<IntArray>, arr2: Array<IntArray>): Array<IntArray> = arr1.mapIndexed { x, arr ->
+        arr.mapIndexed { y, it ->
+            arr2[x][y] + it
+        }.toIntArray()
+    }.toTypedArray()
+}
