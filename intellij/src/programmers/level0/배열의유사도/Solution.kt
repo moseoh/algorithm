2653ac3244625c31f1f3dbca284f3149560ad36c
moseoh <azqazq195@gmail.com>
2023-02-17 21:24:45 +0900

    refactor: Level0 배열의 유사도

--- intellij/src/programmers/level0/배열의유사도/Solution.kt
+++ intellij/src/programmers/level0/배열의유사도/Solution.kt
@@ -1,11 +1,11 @@
 package src.programmers.level0.배열의유사도
 
 fun main(args: Array<String>) {
-    val n = 1234
-    println(Solution().solution(n))
+    val s1 = arrayOf("a", "b", "c")
+    val s2 = arrayOf("com", "b", "d", "p", "c")
+    println(Solution().solution(s1, s2))
 }
 
 class Solution {
-    fun solution(n: Int): Int = n.toString().split("").filter(String::isNotBlank).sumBy { it.toInt() }
-    fun solution2(n: Int): Int = n.toString().toList().sumBy { Character.getNumericValue(it) }
+    fun solution(s1: Array<String>, s2: Array<String>): Int = s1.count(s2::contains)
 }
