ca60ac0df56e1f959204d147e0714a2c3c34a48f
moseoh <azqazq195@gmail.com>
2023-02-17 15:28:54 +0900

    feat: Level0 연속된 수의 합

---
+++ intellij/src/programmers/level0/연속된수의합/Solution.kt
@@ -0,0 +1,14 @@
+package src.programmers.level0.연속된수의합
+
+fun main(args: Array<String>) {
+    val num = 3
+    val total = 12
+    println(Solution().solution(num, total).contentToString())
+}
+
+class Solution {
+    fun solution(num: Int, total: Int): IntArray {
+        var answer: IntArray = intArrayOf()
+        return answer
+    }
+}
