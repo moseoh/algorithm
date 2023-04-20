22146598238f83d90be810b8c9ef076618974e70
moseoh <azqazq195@gmail.com>
2023-04-20 19:10:45 +0900

    feat: Level2 두 원 사이의 정수 쌍

--- intellij/src/programmers/level2/두원사이의정수쌍/Solution.kt
+++ intellij/src/programmers/level2/두원사이의정수쌍/Solution.kt
@@ -1,6 +1,8 @@
 package src.programmers.level2.두원사이의정수쌍
 
-import kotlin.math.pow
+import kotlin.math.ceil
+import kotlin.math.floor
+import kotlin.math.sqrt
 
 fun main() {
     val r11 = 2
@@ -12,22 +14,27 @@ fun main() {
                 "\t- 실행 결과: $result1\n" +
                 "\t- 기댓값: $answer1\n"
     }
+
+    val r12 = 2
+    val r22 = 4
+    val answer2 = 20L
+    val result2 = Solution().solution(r12, r22)
+    check(result2 == answer2) {
+        "\n\n테스트 케이스 2\n" +
+                "\t- 실행 결과: $result2\n" +
+                "\t- 기댓값: $answer2\n"
+    }
 }
 
-class Solution {
-    fun solution(r1: Int, r2: Int): Long {
-        val min = r1.toDouble().pow(2).toInt()
-        val max = r2.toDouble().pow(2).toInt()
 
-        var answer = 0L
-        for (i in 0..r2) {
-            for (j in 0..r2) {
-                val num = (i.toDouble().pow(2) + j.toDouble().pow(2)).toInt()
-                if (num in min..max) {
-                    answer += if (j == 0 || i == 0) 2 else 4
-                }
-            }
+internal class Solution {
+    fun solution(r1: Int, r2: Int): Long {
+        var answer: Long = 0
+        for (i in 1..r2) {
+            val start = ceil(sqrt((r1.toLong() * r1 - i.toLong() * i).toDouble())).toInt()
+            val end = floor(sqrt((r2.toLong() * r2 - i.toLong() * i).toDouble())).toInt()
+            answer += (end - start + 1).toLong()
         }
-        return answer
+        return answer * 4
     }
 }
